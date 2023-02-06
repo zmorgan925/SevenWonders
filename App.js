5e351e069be4a2297bb6a8e419a71aa52d634ece
Zane Morgan <fremen925@yahoo.com>
2023-02-05 21:16:49 -0600

    First Commit

--- App.js
+++ App.js
@@ -1,20 +1,103 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import {
+  Dimensions,
+  Image,
+  Text,
+  ScrollView,
+  StyleSheet,
+  SafeAreaView,
+} from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+import PostContainer from './PostContainer';
+import PhotoViewer from './PhotoViewer';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+SplashScreen.preventAutoHideAsync();
+setTimeout(SplashScreen.hideAsync, 2000);
+
+const image1 = require('./images/wall.png');
+const image2 = require('./images/petra.png');
+const image3 = require('./images/redeemer.png');
+const image4 = require('./images/machu.png');
+const image5 = require('./images/chichen.png');
+const image6 = require('./images/colosseum.png');
+const image7 = require('./images/taj.png');
+
+const timeline = [
+  { title: 'Great Wall of China', image: image1 },
+  { title: 'Petra', image: image2 },
+  { title: 'The Redeemer', image:  image3 },
+  { title: 'Machu Picchu', image: image4 },
+  { title: 'Chichen Itza', image: image5 },
+  { title: 'Colosseum', image: image6 },
+  { title: 'Taj Mahal', image: image7 },
+];
+
+export default class App extends Component {
+  state = {
+    selected: null,
+    position: null,
+  };
+
+  showImage = (selected, position) => {
+    this.setState({
+      selected,
+      position,
+    });
+  }
+
+  closeViewer = () => {
+    this.setState({
+      selected: null,
+      position: null,
+    });
+  }
+
+  renderViewer() {
+    const { selected, position } = this.state;
+
+    if (selected) {
+      return (
+        <PhotoViewer
+          post={selected}
+          position={position}
+          onClose={this.closeViewer}
+        />
+      );
+    }
+  }
+
+  render() {
+    return (
+      <SafeAreaView style={styles.main}>
+        <Text style={styles.toolbar}>Seven Wonders of the World</Text>
+        <ScrollView style={styles.content}>
+        {
+          timeline.map((post, index) =>
+            <PostContainer key={index} post={post}
+            onPress={this.showImage} />
+          )
+        }
+        </ScrollView>
+        {this.renderViewer()}
+      </SafeAreaView>
+    );
+  }
+
 }
 
 const styles = StyleSheet.create({
-  container: {
+  main: {
+    backgroundColor: '#ecf0f1',
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  toolbar: {
+    backgroundColor: '#2c3e50',
+    color: '#fff',
+    fontSize: 22,
+    padding: 20,
+    textAlign: 'center',
+  },
+  content: {
+    flex: 1,
   },
 });
---
+++ PhotoViewer/index.js
@@ -0,0 +1,95 @@
+import React, { Component } from 'react';
+import {
+  Animated,
+  Dimensions,
+  Easing,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+} from 'react-native';
+
+const { width, height } = Dimensions.get('window');
+
+export default class PhotoViewer extends Component {
+  static defaultProps = {
+    onClose: () => {},
+  };
+
+  //componentWillMount() {
+    constructor(props) {
+      super(props);
+    this.animatedValue = new Animated.Value(0);
+  }
+
+  componentDidMount() {
+    Animated.timing(
+      this.animatedValue,
+      {
+        toValue: 1,
+        duration: 400,
+        easing: Easing.in,
+        useNativeDriver: false,
+      }
+    ).start();
+  }
+
+  onPressBtn = () => {
+    this.props.onClose();
+  }
+
+  render() {
+    const { post: { image, title }, position } = this.props;
+    const top = this.animatedValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: [position.pageY, height/2 - position.height/2],
+    });
+    const opacity = this.animatedValue;
+
+    return (
+      <Animated.View
+        style={[
+          styles.main,
+          { opacity },
+        ]}
+      >
+        <Animated.Image
+          source={image}
+          style={[
+            styles.image,
+            { top, opacity }
+          ]}
+        />
+        <TouchableOpacity style={styles.closeBtn}
+          onPress={this.onPressBtn}
+        >
+          <Text style={styles.closeBtnText}>X</Text>
+        </TouchableOpacity>
+      </Animated.View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  main: {
+    backgroundColor: '#000',
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+    right: 0,
+    top: 0,
+  },
+  image: {
+    width,
+    height: 300,
+  },
+  closeBtn: {
+    position: 'absolute',
+    top: 50,
+    right: 20,
+  },
+  closeBtnText: {
+    fontSize: 20,
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+});
